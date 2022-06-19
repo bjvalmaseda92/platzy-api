43d86a8ca031f0105d7886ac490e365d32b00a46
Bábaro Javier Valmaseda Vázquez <bjvalmaseda.g@gmail.com>
2022-06-19 00:23:50 -0400

    feat: ✨ add Category crud

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([ProductSeeder::class]);
+        $this->call([ProductSeeder::class, CategorySeeder::class]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::apiResource("categories", CategoryController::class);
 Route::apiResource("products", ProductController::class);
 
 Route::middleware("auth:sanctum")->get("/user", function (Request $request) {
